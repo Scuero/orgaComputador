554f00fcaf83c34968ca48a6d3aff1541ce76452
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2024-10-22 22:34:36 +0000

    wip: coloreando las celdas del castillo
    Por alguna extraña razón cuando uso los escapes de ANSI no se me
    muestran los caracteres de cada las celdas inicializadas en 'X'.
    Igualmente voy a cambiar la implementación para desvincular los íconos
    del tablero.

--- src/tablero.asm
+++ src/tablero.asm
@@ -19,56 +19,85 @@ celdas:
 %endrep
 %define BYTES_CELDA 2
 %define LONG_TABLERO 7
+%define FILA r13
+%define COLUMNA r12
+%macro macro_rax_offset_celda 0
+	mov rax,FILA
+	imul rax,LONG_TABLERO*BYTES_CELDA
+	mov rcx,COLUMNA
+	imul rcx,BYTES_CELDA
+	add rax,rcx
+%endmacro
 
 newline: db 10,0
 simbolo_string: db " %c ",0
+ansi_castillo: db 27,"[38;5;0;48;5;245m",0
+ansi_resetear: db 27,"[0m",0
 
 
 	section .text
 tablero_inicializar:
+	mov byte [celdas + 46 + celda.es_castillo],1
 	mov byte [celdas + 46 + celda.simbolo_unicode],88
+	mov byte [celdas + 48 + celda.es_castillo],1
 	mov byte [celdas + 48 + celda.simbolo_unicode],88
+	mov byte [celdas + 50 + celda.es_castillo],1
 	mov byte [celdas + 50 + celda.simbolo_unicode],88
 
+	mov byte [celdas + 60 + celda.es_castillo],1
 	mov byte [celdas + 60 + celda.simbolo_unicode],88
+	mov byte [celdas + 62 + celda.es_castillo],1
 	mov byte [celdas + 62 + celda.simbolo_unicode],88
+	mov byte [celdas + 64 + celda.es_castillo],1
 	mov byte [celdas + 64 + celda.simbolo_unicode],88
 	
+	mov byte [celdas + 74 + celda.es_castillo],1
 	mov byte [celdas + 74 + celda.simbolo_unicode],88
+	mov byte [celdas + 76 + celda.es_castillo],1
 	mov byte [celdas + 76 + celda.simbolo_unicode],88
+	mov byte [celdas + 78 + celda.es_castillo],1
 	mov byte [celdas + 78 + celda.simbolo_unicode],88
 
 	ret
 
 
 tablero_imprimir:
-	mov r13,0
+	mov FILA,0
 
 	loop_filas:
-		mov r12,0
+		mov COLUMNA,0
 
 		loop_columnas:
-			; offset fila
-			mov rax,r13
-			imul rax,7*BYTES_CELDA
+			macro_rax_offset_celda		
 
-			; offset columna
-			mov rcx,r12
-			imul rcx,BYTES_CELDA
+			mov r14b,[celdas + rax + celda.es_castillo]
+			cmp r14b,1
+			jne continue_no_iniciar_ansi
 
-			mov rdi,simbolo_string
-			movzx rsi,byte [celdas + rax + rcx + celda.simbolo_unicode]
+			mov rdi,ansi_castillo
 			call printf
 
-			inc r12
-			cmp r12,LONG_TABLERO
+			continue_no_iniciar_ansi:
+			mov rdi,simbolo_string
+			movzx rsi,byte [celdas + rax + celda.simbolo_unicode]
+			call printf
+
+			cmp r14b,1
+			jne continue_no_resetear_ansi
+
+			mov rdi,ansi_resetear
+			call printf
+
+			continue_no_resetear_ansi:
+			inc COLUMNA
+			cmp COLUMNA,LONG_TABLERO
 			jl loop_columnas
 
 		mov rdi,newline
 		call printf
 
-		inc r13
-		cmp r13,LONG_TABLERO
+		inc FILA
+		cmp FILA,LONG_TABLERO
 		jl loop_filas
 
 	ret
